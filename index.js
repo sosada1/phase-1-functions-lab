0d7098be545bde3c282859df85dbcf27853d9912
Adam <adam.l.sosa@gmail.com>
2022-04-27 11:39:01 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location > 42) { 
+       return location - 42; 
+    } else {
+       return 42 - location;
+    }
+
+}
+function distanceFromHqInFeet(){
+   if ()
+}
