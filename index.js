3475184c70bf64eb4f62a5ffbbc7931e0d631c79
Adam <adam.l.sosa@gmail.com>
2022-04-27 20:28:56 -0400

    index.js

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ function distanceFromHqInFeet(location){
 }
 
 function distanceTravelledInFeet(start, destination){
-   console.log (distanceTravelledInFeet(start, destination));
    if (destination > start) {
-   return destination - start * 264;
+   return (destination - start) * 264;
    } else {
-      return start - destination * 264;
+      return (start - destination) * 264;
    }  
 }
+
+function calculatesFarePrice(start, destination){
+   if (destination - start <= 400) {
+   return 'gives customer a free sample';
+   } else if (destination - start > 400 && desination < 2000) {
+      return "2 cents per foot, not including the first 400";
+   } else if (destination - start > 2000 && destination < 2500) {
+      return 'charges 25 dollars for distance over 2000 feet';
+   } else if (destination -start > 2500) {
+      return 'cannot travel that far';
+   }
+}
+
 
