646ec07cdaf31d90d2ee42668190ed7dcc1c123e
Adam <adam.l.sosa@gmail.com>
2022-06-12 16:35:42 -0400

    index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(location){
-    if (location > 42) { 
-       return location - 42; 
-    } else {
-       return 42 - location;
-    }
 
-}
-function distanceFromHqInFeet(location){
-   console.log (distanceFromHqInBlocks(location)* 264);
-   return distanceFromHqInBlocks(location) * 264
-}
 
-function distanceTravelledInFeet(start, destination){
-   if (destination > start) {
-   return (destination - start) * 264;
-   } else {
+function distanceFromHqInBlocks(block) {
+   // returns the number of blocks given a value
+if (block> 42) {
+   console.log(block)
+   return block - 42 ;
+} else { 
+   console.log(block)
+   return 42 - block;
+}}
+
+function distanceFromHqInFeet(block){     
+   return distanceFromHqInBlocks(block) * 264;  
+   }
+   //call the distanceFromHqInBlocks function from inside he distancefromHqInFeet function, 
+   // passing the argument from distanceFromHqIn Feet into distanceFromHqInBlocks
+   // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  
+   function distanceTravelledInFeet(start, destination){     
+   //returns the number of feet traveled
+   
+   if (start > destination) {
       return (start - destination) * 264;
-   }  
-}
-
-function calculatesFarePrice(start, destination){
-   if (destination - start <= 400) {
-   return 'gives customer a free sample';
-   } else if (destination - start > 400 && desination < 2000) {
-      return "2 cents per foot, not including the first 400";
-   } else if (destination - start > 2000 && destination < 2500) {
-      return 'charges 25 dollars for distance over 2000 feet';
-   } else if (destination -start > 2500) {
-      return 'cannot travel that far';
+   }else {
+      return (destination - start) * 264;
    }
 }
-
+function calculatesFarePrice(start, destination) {
+   // returns the fare for the customer 
+let distance = distanceTravelledInFeet(start, destination);
+console.log(distance);
+ 
+   if (distance < 400) {
+   return (0);
+   }if (distance > 2500){
+   return ('cannot travel that far');
+   }if (distance >2000){
+   return (25);   
+   }if (distance < 2000) {
+   return ( distance - 400) *.02;
+   }
+}
 
